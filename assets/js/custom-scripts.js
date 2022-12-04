79a7b03e9caed5a27ca9489128bc487cd6237802
Parth Parmar <71545063+parth-r-parmar@users.noreply.github.com>
2022-12-04 23:03:51 +0530

    Update custom-scripts.js

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -44,7 +44,7 @@ $("body").swiperight(function() {
     $(".navbar-toggler").addClass("active");
 });
 
-navbarSupportedContent.swipeleft(function() {
+$("body").swiperleft(function() {
     if (navbarSupportedContent.hasClass("active")) {
        overlay.addClass("active");
        toggleSwitch.removeClass("open");
